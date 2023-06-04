d6bd594e25ed02e025806690080a5e6f383cced4
awesomepotato2016 <awesomepotato2016@gmail.com>
2023-06-04 11:02:38 -0400

    updated minMax

--- homeworks/BasicJava1/MinMax.java
+++ homeworks/BasicJava1/MinMax.java
@@ -1,7 +1,30 @@
 class MinMax {
     public static int secondMax(int[] numbers ){
-        
+        int prevMax = Integer.MIN_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for(int i=0; i < numbers.length; i++){
+            if(numbers[i] > max){
+                prevMax = max;
+                max = numbers[i];
+            }
+        }
+        return prevMax;
     }
 
-    public static int secondMin(int[] numbers );
+    public static int secondMin(int[] numbers ){
+        int prevMin = Integer.MAX_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for(int i=0; i < numbers.length; i++){
+            if(numbers[i] < min){
+                prevMin = min;
+                min = numbers[i];
+            }
+        }
+        return prevMin;
+    }
+    public static void main(String[] args){
+        
+    }
 }
